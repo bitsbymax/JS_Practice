b72942a87c71e8a2a87ffe59464da6794fb7e4d0
Maksym Vikolash <vikolashmaxim@mycubes.nl>
2023-02-12 16:12:29 +0200

    new info

---
+++ JS_ Examples/Functions/f_call_without().js
@@ -0,0 +1,12 @@
+alert`1337`;
+throw ((onerror = alert), 1337);
+Function`x${'alert\x281337\x29'}x```;
+'alert\x281337\x29' instanceof { [Symbol['hasInstance']]: eval };
+valueOf = alert;
+window + '';
+x = new DOMMatrix();
+matrix = alert;
+x.a = 1337;
+location = 'javascript' + ':' + x;
+// or any DOMXSS sink such as location=name
+//https://portswigger.net/research/the-seventh-way-to-call-a-javascript-function-without-parentheses
