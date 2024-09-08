1fcc8badafc71a75a2ac5b57798baaa9e6513af5
Maksym Vikolash <vikolashmaxim@gmail.com>
2024-09-08 17:23:11 +0300

    move to another repo

--- JS_Practice/sum.js
+++
@@ -1,9 +0,0 @@
-function sum(...args) {
-  console.log(args)
-  if (args.length > 1) {
-    return args.reduce((a, b) => a + b)
-  }
-
-  return (v) => v + +args.concat();
-}
-console.log(sum(1)(4))
